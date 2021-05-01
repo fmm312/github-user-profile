68936fafd579a155de857d49c08225c107e87905
Felipe <fmm312@gmail.com>
2021-05-01 20:55:42 -0300

    Atualizando readme

--- src/components/Charts/Pie.jsx
+++ src/components/Charts/Pie.jsx
@@ -10,7 +10,7 @@ const PieChart = () => {
 
   function countLanguages() {
     const repositories = JSON.parse(sessionStorage.getItem('userRepositories'));
-    const filteredArray = repositories.map(item => item.language).slice(0, 5);
+    const filteredArray = repositories.map(item => item.language);
 
     let count = {};
     
